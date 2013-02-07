bd527a0f0af6f39d1c6d093f0f7105f85c9d3063
fpumir <fpumir@terredeweb.fr>
2013-02-07 16:36:02 +0100

    geocode on travel ; new methods in admin controller
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/Admin_controller.php
+++ app/Controllers/Admin_controller.php
@@ -11,5 +11,28 @@ class Admin_controller{
     echo Views::instance()->render('admin/travels.html');
   }
   
+  function create(){
+    switch(F3::get('VERB')){
+      case 'GET':
+        echo Views::instance()->render('admin/travel.html');
+      break;
+      case 'POST':
+      
+      break;
+    }
+  }
+  
+  function edit(){
+    switch(F3::get('VERB')){
+      case 'GET':
+      
+      break;
+      case 'POST':
+      
+      break;
+    }
+  }
+  
+  
 }
 ?>
