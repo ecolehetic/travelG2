eefae18cf21c19b5ad8d2cd52da52ebc3708a1af
fpumir <fpumir@terredeweb.fr>
2013-02-07 17:34:16 +0100

    create implement
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -11,5 +11,28 @@ class Admin extends Prefab{
     return $location->find();
   }
   
+  function create(){
+    $location=new DB\SQL\Mapper(F3::get('dB'),'location');
+    $location->copyFrom('POST');
+    $location->save();
+    
+    $image=Web::instance()->receive();
+    if($image){
+      $this->_resize($image,1024,768);
+      $picture=new DB\SQL\Mapper(F3::get('dB'),'pictures');
+      $picture->src='min_'.$image;
+      $picture->idLocation=$location->id;
+      $picture->save();
+    }
+  }
+  
+  protected function _resize($image,$width,$height){
+    $img=new Image(F3::get('UPLOADS').$image,true);
+    $img->resize($width,$height);
+    file_put_contents(F3::get('UPLOADS').'min_'.$image,$img->dump('jpeg'));
+  }
+  
+  
+  
 }
 ?>
